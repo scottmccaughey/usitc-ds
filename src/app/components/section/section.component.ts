96a8b5a351188f5013a5f55fea10023b98232804
Scott McCaughey <scott@scottmccaughey.com>
2018-08-06 13:10:56 -0400

    Building styleguide

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -14,13 +14,15 @@ export class SectionComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    const secSplit = this.section['referenceURI'].split('-');
     const depth = this.section['depth'] > 1 ? this.section['depth'] - 1 : 1;
     this.content['header'] = '<h' + depth + '>' + this.section['header'] + '</h' + depth + '>';
     this.content['description'] = this.section['description'];
-    this.content['sourceFile'] = environment._devAssetsLocation + this.section['source']['filename'];
-    this.content['sourceLine'] = this.section['source']['line'];
+    this.content['markupClass'] = 'kss-section-' + secSplit[1];
     this.content['markup'] = this.section['markup'];
     this.content['modifiers'] = this.section['modifiers'];
     this.content['parameters'] = this.section['parameters'];
+    this.content['sourceFile'] = environment._devAssetsLocation + this.section['source']['filename'];
+    this.content['sourceLine'] = this.section['source']['line'];
   }
 }
