1fda8fa786f96c6d96a9601ad91d1fb67a81f84a
Scott McCaughey <scott@scottmccaughey.com>
2018-10-11 14:42:55 -0400

    Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { ClipboardModule } from 'ngx-clipboard';
 import { PrismModule } from '@ngx-prism/core';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,16 +15,16 @@ import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.componen
 import { CodeComponent } from './components/code/code.component';
 import { ColorsComponent } from './components/colors/colors.component';
 import { ErrorComponent } from './components/error/error.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { IconComponent } from './components/icon/icon.component';
 import { IconsComponent } from './components/icons/icons.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavComponent } from './components/nav/nav.component';
 import { ParametersComponent } from './components/parameters/parameters.component';
 import { SectionComponent } from './components/section/section.component';
 import { StyleguideComponent } from './components/styleguide/styleguide.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -32,24 +33,24 @@ import { LayoutComponent } from './components/layout/layout.component';
     CodeComponent,
     ColorsComponent,
     ErrorComponent,
+    FooterComponent,
     HeaderComponent,
     HomeComponent,
     IconComponent,
     IconsComponent,
     KeepHtmlPipe,
+    LayoutComponent,
+    NavComponent,
     ParametersComponent,
     SectionComponent,
-    StyleguideComponent,
-    NavComponent,
-    FooterComponent,
-    LayoutComponent
+    StyleguideComponent
   ],
   imports: [
-    AppRoutingModule,
     AppRoutingModule,
     BrowserModule,
     ClipboardModule,
     HttpClientModule,
+    InlineSVGModule.forRoot(),
     PrismModule
   ],
   exports: [StyleguideComponent],
