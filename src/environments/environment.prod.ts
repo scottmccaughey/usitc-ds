57248c98eb8da35a15e1fb7ea2c869c991ee5a64
Scott McCaughey <scott@scottmccaughey.com>
2018-10-25 14:07:08 -0400

    Styling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,26 @@
+//                                888
+//                                888
+//                                888
+//  88888b.  888d888 .d88b.   .d88888
+//  888 "88b 888P"  d88""88b d88" 888
+//  888  888 888    888  888 888  888
+//  888 d88P 888    Y88..88P Y88b 888
+//  88888P"  888     "Y88P"   "Y88888
+//  888
+//  888
+//  888
+//
+// These are environment configuration settings that are used when running `ng build` or
+// `ng serve`. If you do not specify a configuration, the dev configuration is used.
+// The full configuration setup can be found in `angular.json`.
+//
+// Configuration    File                    Serve Command           Build Command
+// ------------------------------------------------------------------------------------------
+//   dev            environment.ts          `ng serve`              `ng build`
+// > prod           environment.prod.ts     `ng serve -c=prod`      `ng build -c=prod`
+
 export const environment = {
   production: true,
-
+  DEBUG: false,
   _devAssetsLocation: 'src/assets-dev/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,26 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// 888                            888          d88P         888                        888
+// 888                            888         d88P          888                        888
+// 888                            888        d88P           888                        888
+// 888  .d88b.   .d8888b  8888b.  888       d88P    .d8888b 888  .d88b.  888  888  .d88888
+// 888 d88""88b d88P"        "88b 888      d88P    d88P"    888 d88""88b 888  888 d88" 888
+// 888 888  888 888      .d888888 888     d88P     888      888 888  888 888  888 888  888
+// 888 Y88..88P Y88b.    888  888 888    d88P      Y88b.    888 Y88..88P Y88b 888 Y88b 888
+// 888  "Y88P"   "Y8888P "Y888888 888   d88P        "Y8888P 888  "Y88P"   "Y88888  "Y88888
+//
+//
+//
+//
+// These are environment configuration settings that are used when running `ng build` or
+// `ng serve`. If you do not specify a configuration, the dev configuration is used.
+// The full configuration setup can be found in `angular.json`.
+//
+// Configuration    File                    Serve Command           Build Command
+// ------------------------------------------------------------------------------------------
+// > dev            environment.ts          `ng serve`              `ng build`
+//   prod           environment.prod.ts     `ng serve -c=prod`      `ng build -c=prod`
 
 export const environment = {
   production: false,
-
+  DEBUG: false,
   _devAssetsLocation: 'src/assets-dev/'
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
