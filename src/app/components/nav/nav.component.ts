a9cbb9caa40eaf8c5f652c52ced74ee0f4c5bb88
Scott McCaughey <scott@scottmccaughey.com>
2018-11-20 16:03:29 -0500

    Attempting to fix scrolling/printing issues

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { ConfigService } from '../../services/config.service';
+import { ConfigService } from "../../services/config.service";
 
 @Component({
-  selector: 'ds-nav',
-  templateUrl: './nav.component.html'
+  selector: "ds-nav",
+  templateUrl: "./nav.component.html"
 })
 export class NavComponent implements OnInit {
   kss: object;
   menu = [];
   route: string;
+  navOpen;
 
-  constructor(private config: ConfigService) {
-    this.config.kss.subscribe((kss) => (this.kss = kss));
-    this.config.route.subscribe((route) => (this.route = route));
+  constructor(public config: ConfigService) {
+    this.config.kss.subscribe(kss => (this.kss = kss));
+    this.config.route.subscribe(route => (this.route = route));
+    this.config.navOpen.subscribe(navOpen => (this.navOpen = navOpen));
   }
 
   ngOnInit() {
@@ -24,19 +26,19 @@ export class NavComponent implements OnInit {
   buildMenu() {
     const home = {};
 
-    home['name'] = 'Home';
-    home['url'] = '';
-    home['order'] = 0;
-    home['child'] = false;
+    home["name"] = "Home";
+    home["url"] = "";
+    home["order"] = 0;
+    home["child"] = false;
     this.menu.push(home);
 
-    this.kss['sections'].forEach((section) => {
+    this.kss["sections"].forEach(section => {
       const obj = {};
 
-      obj['name'] = section.header;
-      obj['url'] = '/' + section.referenceURI.replace(/-/g, '/');
-      obj['order'] = section.referenceNumber;
-      obj['child'] = section.depth > 1 ? true : false;
+      obj["name"] = section.header;
+      obj["url"] = "/" + section.referenceURI.replace(/-/g, "/");
+      obj["order"] = section.referenceNumber;
+      obj["child"] = section.depth > 1 ? true : false;
 
       if (section.depth <= 2) {
         this.menu.push(obj);
